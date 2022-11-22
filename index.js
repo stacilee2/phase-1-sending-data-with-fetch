862c9150b8bf883066e1b61ce58eec51beff20e5
Staci Rosales <stacileep2@gmail.com>
2022-11-22 14:04:18 -0600

    commit tree 821029a10a3126b37fd58ea4b79e8f4b10239a1c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+// The destination URL
+// Headers for 'Content-Type' and 'Accept', both set to 'application/json'
+// A body with the name and email passed in as arguments to submitData. 
+// These should be assigned to name and 
+// email keys within an object. This object should then be stringified.
+
+  
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({name: name, email: email}),
+    };
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+    return response.json();
+    })
+    .then(function (object) {
+        document.body.innerHTML = object.id
+    })
+    .catch(function (error) {
+        alert("Unauthorized Access");
+        document.body.innerHTML = error.message;
+      });
+}
